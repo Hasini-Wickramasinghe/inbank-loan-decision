43ed7892fc7229b47516f11d92c0c7f8c824ee96
Hasini-Wickramasinghe <hasiniwickramasinghe.hw@gmail.com>
2026-03-24 17:24:55 +0200

    input validation for decimal for months and amounts

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,9 +10,18 @@ app.post("/api/loan", (req, res) => {
     const { personalCode, amount, period } = req.body;
 
     // validation
-    if (!personalCode || !amount || !period) {
+    if (!personalCode || amount == null || period == null) {
         return res.status(400).json({ error: "Missing fields" });
     }
+
+    if (typeof amount !== "number" || typeof period !== "number"){
+        return res.status(400).json({error: "Amount and period must be numbers"});
+    }
+
+    if (!Number.isInteger(amount) || !Number.isInteger(period)){
+        return res.status(400).json({error: "Amount and period must be whole numbers"});
+    }
+
 
     if (amount < 2000 || amount > 10000 || period < 12 || period > 60) {
         return res.status(400).json({ error: "Invalid amount or period" });
