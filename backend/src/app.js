8045df0b2bcbd87dce27601a7c1813cef5512d4f
Hasini-Wickramasinghe <hasiniwickramasinghe.hw@gmail.com>
2026-03-24 17:13:56 +0200

    defined route and error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const {evaluateLoan} = require('./decisionEngine')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/loan", (req, res) => {
+    const { personalCode, amount, period } = req.body;
+
+    // validation
+    if (!personalCode || !amount || !period) {
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
+    if (amount < 2000 || amount > 10000 || period < 12 || period > 60) {
+        return res.status(400).json({ error: "Invalid amount or period" });
+    }
+
+    const result = evaluateLoan(personalCode, amount, period);
+    res.json(result);
+});
